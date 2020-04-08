f5700bbc37cf53efef530553b50b52aab64c07d7
Sammer Valgas <sammer.valgas@gmail.com>
2020-04-08 20:11:27 -0300

    Update DeleteBuildItems.groovy

--- jobs_builds/DeleteBuildItems.groovy
+++ jobs_builds/DeleteBuildItems.groovy
@@ -3,3 +3,17 @@ def job = Jenkins.instance.getItem(jobName)
 job.getBuilds().each { it.delete() }  
 job.nextBuildNumber = 1   
 job.save()
+
+// MULTI BRANCH PIPELINE
+def branchName = 'BRANCH_NAME'
+def job = Jenkins.instance.getItemByFullName(jobName).getBranch(branchName)
+job.getBuilds().each { it.delete() }  
+job.nextBuildNumber = 1   
+job.save()
+
+// Abort
+Jenkins.instance
+.getItemByFullName("<JOB NAME>")
+.getBranch("<BRANCH NAME>")
+.getBuildByNumber(<BUILD NUMBER>)
+.finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"));
