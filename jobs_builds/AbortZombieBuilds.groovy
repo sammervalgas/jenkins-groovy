a578ed15d1f7f9ddfec6c3018f28347cf1849564
Sammer Valgas <sammer.valgas@gmail.com>
2020-04-08 20:14:36 -0300

    Update AbortZombieBuilds.groovy

--- jobs_builds/AbortZombieBuilds.groovy
+++ jobs_builds/AbortZombieBuilds.groovy
@@ -1,5 +1,12 @@
+// MultiBranch
 Jenkins.instance
 .getItemByFullName("<JOB NAME>")
 .getBranch("<BRANCH NAME>")
 .getBuildByNumber(<BUILD NUMBER>)
 .finish(hudson.model.Result.ABORTED, new java.io.IOException("Aborting build"));
+
+// Simple
+Jenkins.instance
+.getItemByFullName("JobName")
+.getBuildByNumber(JobNumber)
+.finish(hudson.model.Result.ABORTED,new java.io.IOException("Aborting build"));
